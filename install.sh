11a8d69062f6e5353929ba44289e23e2fca23669
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2025-09-18 12:47:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo Installing pip
 sudo apt install python3-pip -y
 
 echo Installing virtual environment and packages
-cd LiveTradingBots/code
+cd python_crypto_bot/code
 sudo apt-get install python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
