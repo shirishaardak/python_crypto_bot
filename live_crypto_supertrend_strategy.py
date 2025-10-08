3e6309f253561c94ecd03bbb436f4f74a715b4b6
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2025-10-08 07:22:57 +0530

    Update live_crypto_supertrend_strategy.py

--- live_crypto_supertrend_strategy.py
+++ live_crypto_supertrend_strategy.py
@@ -70,7 +70,7 @@ renko_param = {
 # ---------------------------------------
 # Candle Fetch
 # ---------------------------------------
-def fetch_and_save_delta_candles(symbol, resolution='15m', days=7, save_dir='.', tz='Asia/Kolkata'):
+def fetch_and_save_delta_candles(symbol, resolution='1h', days=7, save_dir='.', tz='Asia/Kolkata'):
     headers = {'Accept': 'application/json'}
     start = int((datetime.now() - timedelta(days=days)).timestamp())
     params = {
@@ -103,7 +103,7 @@ def fetch_and_save_delta_candles(symbol, resolution='15m', days=7, save_dir='.',
 # Process symbol
 # ---------------------------------------
 def process_symbol(symbol, renko_param, ha_save_dir="./data/crypto"):
-    df = fetch_and_save_delta_candles(symbol, resolution='5m', days=7, save_dir=ha_save_dir)
+    df = fetch_and_save_delta_candles(symbol, resolution='1h', days=7, save_dir=ha_save_dir)
     if df is None or df.empty:
         return renko_param
 
@@ -241,7 +241,7 @@ while True:
                             size=ORDER_QTY,
                             side='sell',
                             order_type=OrderType.MARKET,
-                            stop_price=EMA_21_DN,
+                            stop_price=EMA_21,
                         )
                         
                         if trailing_stop_order_buy:
@@ -328,7 +328,7 @@ while True:
                             size=ORDER_QTY,
                             side='buy',
                             order_type=OrderType.MARKET,
-                            stop_price=EMA_21_UP,
+                            stop_price=EMA_21,
                         )
                         
                         if trailing_stop_order_sell:
