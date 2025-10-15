0cffea1dbf086a5f23a44af7d4f549bba1c29077
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2025-10-15 20:59:29 +0530

    Update live_crypto_supertrend_strategy.py

--- live_crypto_supertrend_strategy.py
+++ live_crypto_supertrend_strategy.py
@@ -122,8 +122,8 @@ def process_symbol(symbol, renko_param, ha_save_dir="./data/crypto"):
 
     # Trade signals
     df['single'] = 0
-    df.loc[(df['HA_close'] > df['EMA_21_UP']) & (df['HA_close'] > df['HA_open'].shift(1)), 'single'] = 1
-    df.loc[(df['HA_close'] < df['EMA_21_UP']) & (df['HA_close'] < df['HA_open'].shift(1)), 'single'] = -1
+    df.loc[(df['HA_close'] > df['EMA_21_UP']) & (df['HA_close'] > df['HA_close'].shift(1)) & (df['HA_close'] > df['HA_open']), 'single'] = 1
+    df.loc[(df['HA_close'] < df['EMA_21_UP']) & (df['HA_close'] < df['HA_close'].shift(1)) & (df['HA_close'] < df['HA_open']), 'single'] = -1
 
     # Save for debugging/backtest
     os.makedirs(ha_save_dir, exist_ok=True)
