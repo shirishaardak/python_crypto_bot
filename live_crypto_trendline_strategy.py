f8522525da1075aabb13b380c1cf7e08d8425fc5
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2025-10-28 09:33:56 +0530

    Update live_crypto_trendline_strategy.py

--- live_crypto_trendline_strategy.py
+++ live_crypto_trendline_strategy.py
@@ -29,7 +29,7 @@ load_dotenv()
 
 BASE_URL = "https://api.india.delta.exchange"
 SYMBOLS = ["BTCUSD", "ETHUSD"]
-ORDER_QTY = {"BTCUSD": 1, "ETHUSD": 1}
+ORDER_QTY = {"BTCUSD": 10, "ETHUSD": 10}
 TREND_INTERVAL = "15m"
 TREND_DAYS = 3
 DRY_RUN = False  # ⚠️ Set False for live trading
