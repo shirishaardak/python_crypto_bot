60a1d93709ca7b3faaf91ef4192ee11ca0f304bf
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2025-10-30 09:25:22 +0530

    Update live_crypto_trendline_strategy.py

--- live_crypto_trendline_strategy.py
+++ live_crypto_trendline_strategy.py
@@ -149,9 +149,8 @@ def process_symbol(symbol, renko_param, ha_save_dir="./data/live_crypto_supertre
     df["Trendline"] = df["Trendline"].ffill()
     # Trade signals
     df['single'] = 0
-    df.loc[(df['HA_close'] > df['Trendline']) & (df['HA_close'].shift(1) < df['Trendline']), 'single'] = 1
-    df.loc[(df['HA_close'] < df['Trendline']) & (df['HA_close'].shift(1) > df['Trendline']), 'single'] = -1
-    print(df)
+    df.loc[(df['HA_close'] > df['Trendline']) & (df['HA_close'] > df['HA_close'].shift(1)) & (df['HA_close'] > df['HA_open']), 'single'] = 1
+    df.loc[(df['HA_close'] < df['Trendline']) & (df['HA_close'] < df['HA_close'].shift(1)) & (df['HA_close'] < df['HA_open']), 'single'] = -1
     # os.makedirs(ha_save_dir, exist_ok=True)
     # df.to_csv(f"{ha_save_dir}/supertrend_live_{symbol}.csv")
 
@@ -224,7 +223,7 @@ while True:
     try:
         now = datetime.now()
 
-        if now.second == 10 and datetime.now().minute % 5 == 0:
+        if now.second == 10 and datetime.now().minute % 15 == 0:
             log(f"\n[{now.strftime('%Y-%m-%d %H:%M:%S')}] Running cycle...")
 
             # Process symbols
