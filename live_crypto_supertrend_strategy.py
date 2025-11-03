26363f9ccd09266c004749d275d324893795d71a
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2025-11-03 22:22:20 +0530

    Update live_crypto_supertrend_strategy.py

--- live_crypto_supertrend_strategy.py
+++ live_crypto_supertrend_strategy.py
@@ -134,7 +134,7 @@ def fetch_and_save_delta_candles(symbol, resolution='5m', days=7, save_dir='.', 
 # Process symbol
 # ---------------------------------------
 def process_symbol(symbol, renko_param, ha_save_dir="./data/live_crypto_supertrend_strategy"):
-    df = fetch_and_save_delta_candles(symbol, resolution='15m', days=7, save_dir=ha_save_dir)
+    df = fetch_and_save_delta_candles(symbol, resolution='5m', days=7, save_dir=ha_save_dir)
     if df is None or df.empty:
         return renko_param
 
@@ -144,7 +144,7 @@ def process_symbol(symbol, renko_param, ha_save_dir="./data/live_crypto_supertre
     df = ta.ha(open_=df['open'], high=df['high'], close=df['close'], low=df['low'])
     df['EMA_21'] = ta.ema(df['HA_close'], length=5)
 
-    offset = 300 if symbol == "BTCUSD" else 20
+    offset = 300 if symbol == "BTCUSD" else 30
     df['EMA_21_UP'] = df['EMA_21'] + offset
     df['EMA_21_DN'] = df['EMA_21'] - offset
 
