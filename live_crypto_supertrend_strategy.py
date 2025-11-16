bc942a93318115c4ef815c950589a4359e836bda
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2025-11-16 21:47:43 +0530

    Update live_crypto_supertrend_strategy.py

--- live_crypto_supertrend_strategy.py
+++ live_crypto_supertrend_strategy.py
@@ -340,7 +340,7 @@ while True:
 
             df_status = pd.DataFrame.from_dict(renko_param, orient='index')
             print("\nCurrent Strategy Status:")
-            print(df_status[['Date', 'close_15m', 'EMA_21_UP', 'EMA_21_DN', 'EMA_21', 'option', 'single', 'pnl']])
+            print(df_status[['Date', 'EMA_21_UP', 'EMA_21_DN', 'EMA_21', 'option', 'single', 'pnl']])
 
             print("Waiting for next cycle...\n")
             t.sleep(55)
