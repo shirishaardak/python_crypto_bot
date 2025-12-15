0c9fdceaad2b4ff4c7eb4c24bf0a190066ea14f8
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2025-12-15 10:47:25 +0530

    ha['max_high'] = ha['max_high'].ffill().bfill()     ha['max_low']  = ha['max_low'].ffill().bfill()
    ha['max_high'] = ha['max_high'].ffill().bfill()
        ha['max_low']  = ha['max_low'].ffill().bfill()

--- trade_reversal_update.py
+++ trade_reversal_update.py
@@ -120,8 +120,8 @@ def calculate_trendline(df):
     if len(min_idx) > 0:
         ha.loc[min_idx, 'max_low'] = ha.loc[min_idx, 'HA_low']
 
-    ha['max_high'].fillna(method='ffill', inplace=True)
-    ha['max_low'].fillna(method='ffill', inplace=True)
+    ha['max_high'] = ha['max_high'].ffill().bfill()
+    ha['max_low']  = ha['max_low'].ffill().bfill()
 
     ha['Trendline'] = np.nan
     if len(ha) == 0:
