f0c88b4b6f9eecccf7faabc8d78980f38bd945df
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2026-02-14 05:56:44 +0530

    Update bn_trend_following_strategy.py

--- bn_trend_following_strategy.py
+++ bn_trend_following_strategy.py
@@ -52,8 +52,8 @@ TRADES_FILE=f"{folder}/live_trades.csv"
 TOKEN_FILE="auth/api_key/access_token.txt"
 
 # ================= TELEGRAM =================
-TELEGRAM_BOT_TOKEN="TEL_BOT_TOKEN"
-TELEGRAM_CHAT_ID="TEL_CHAT_ID"
+TELEGRAM_TOKEN = os.getenv("TEL_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TEL_CHAT_ID")
 
 def send_telegram(msg):
     try:
