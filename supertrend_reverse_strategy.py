584f11a6228684f62d6556e69333ac09da680699
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2026-03-21 10:55:10 +0530

    tken add

--- supertrend_reverse_strategy.py
+++ supertrend_reverse_strategy.py
@@ -12,8 +12,8 @@ load_dotenv()
 
 # ================= TELEGRAM =================
 
-TELEGRAM_TOKEN = os.getenv("TEL_BOT_TOKE")
-TELEGRAM_CHAT_ID = os.getenv("TEL_CHAT_I")
+TELEGRAM_TOKEN = os.getenv("TEL_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TEL_CHAT_ID")
 _last_tg = {}
 
 def send_telegram(msg, key=None, cooldown=30):
