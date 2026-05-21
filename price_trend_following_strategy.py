7635dbafc55de06737d7ccff01f18334bb7f2744
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2026-05-21 20:33:37 +0530

    update all code

--- price_trend_following_strategy.py
+++ price_trend_following_strategy.py
@@ -62,7 +62,7 @@ utils = TradingUtils(
     taker_fee=TAKER_FEE,
     timeframe=TIMEFRAME,
     days=DAYS,
-    telegram_token=os.getenv("testmyaglostrateg"),
+    telegram_token=os.getenv("price_trend_following_strategy"),
     telegram_chat_id=os.getenv("TELEGRAM_CHAT_ID"),
     bot_name=BOT_NAME
 )
